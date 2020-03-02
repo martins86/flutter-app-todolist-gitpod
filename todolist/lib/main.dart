6dd9fa45d71fc8d06f0137f9e9c07f61baad6db5
martins86 <martins86@gmail.com>
2020-03-02 01:15:49 +0000

    update name display

--- todolist/lib/main.dart
+++ todolist/lib/main.dart
@@ -7,11 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'To Do: List',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'To Do: List'),
     );
   }
 }
