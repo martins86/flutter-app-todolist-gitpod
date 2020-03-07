0a3621bfc810f5fa08bc1c1e21c91ba38e458fa9
martins86 <martins86@gmail.com>
2020-03-07 21:31:19 +0000

    Include json to dart

--- todolist/lib/models/item.dart
+++ todolist/lib/models/item.dart
@@ -4,4 +4,17 @@ class Item {
 
   Item({this.title, this.done});
 
+  // From Json => github.io/json_to_dart/
+  Item.fromJson(Map<String, dynamic> json) {
+    title = json['title'];
+    done = json['done'];
+  }
+
+  // To Json => github.io/json_to_dart/
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['title'] = this.title;
+    data['done'] = this.done;
+    return data;
+  }
 }
