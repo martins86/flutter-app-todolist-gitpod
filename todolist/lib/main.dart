ce1c16f244b6b1fdfbc2fa55d8ec8f66435d8bc7
martins86 <martins86@gmail.com>
2020-03-07 21:33:10 +0000

    add AppBar

--- todolist/lib/main.dart
+++ todolist/lib/main.dart
@@ -19,6 +19,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("To Do: List"),
+      ),
       body: Container(
         child: Container(
           child: Center(
